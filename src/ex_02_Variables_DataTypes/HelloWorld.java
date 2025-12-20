71c9b994651fa0b9d93cef4d55fa8e7ce15523e0
Ashit Datey <dateyashit73@gmail.com>
2025-12-20 12:19:06 +0530

    Renamed the Folder

--- src/ex_02_Variables_DataTypes/HelloWorld.java
+++ src/ex_02_Variables_DataTypes/HelloWorld.java
@@ -1,7 +1,8 @@
-package ex_02_Java_Basics;
+package ex_02_Variables_DataTypes;
 
 public class HelloWorld {
     public static void main(String[] args) {
+
         System.out.println("Hello Ashit D");
     }
 }
--- src/ex_02_Variables_DataTypes/Test_Git.java
+++ src/ex_02_Variables_DataTypes/Test_Git.java
@@ -1,4 +1,4 @@
-package ex_02_Java_Basics;
+package ex_02_Variables_DataTypes;
 
 public class Test_Git {
     public static void main(String[] args) {
